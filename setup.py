5e74a37b2b736ba8ff8b2698a398f5d2afd16fe1
Tom Kelly <tomk10tk@gmail.com>
2022-01-14 13:44:33 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ py_versions = "2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3
 
 requirements = [
     "numba==0.55.0",
-    "numpy>=1.22.0",
+    "numpy>=1.21.0",
     "pandas<=1.2.5",
     "pathlib",
     "matplotlib",
