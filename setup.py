4b260b97ae28f619551496d0760c7c5379e601a9
Francisco Puig Pérez-Barquero <53043760+pacs27@users.noreply.github.com>
2022-04-04 19:32:51 +0200

    numpy should not be changed

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ py_versions = "2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3
 
 requirements = [
     "numba==0.55.0",
-    "numpy>=1.18.0,<=1.22.3",
+    "numpy>=1.18.0,<1.22",
     "pandas>=1.4.0",
     "pathlib",
     "matplotlib",
