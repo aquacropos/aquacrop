01d34e87386901d6bc377b084fab65b1ca04a3aa
thomasdkelly <tomk10tk@gmail.com>
2022-05-30 11:38:24 +0100

    allow weather and sim start and end dates to allign

--- aquacrop/initialize/read_weather_inputs.py
+++ aquacrop/initialize/read_weather_inputs.py
@@ -25,12 +25,12 @@ def read_weather_inputs(clock_sctruct, weather_df):
     start_date = clock_sctruct.simulation_start_date
     end_date = clock_sctruct.simulation_end_date
 
-    if weather_df.Date.iloc[0] >= start_date:
+    if weather_df.Date.iloc[0] > start_date:
         raise ValueError(
             "The first date of the climate data cannot be longer than the start date of the model."
         )
 
-    if weather_df.Date.iloc[-1] <= end_date:
+    if weather_df.Date.iloc[-1] < end_date:
         raise ValueError(
             "The model end date cannot be longer than the last date of climate data."
         )
