a2ab48ee89c49580a20b5f7def6250d972ebe37e
pacs27 <pacopuig1995@gmail.com>
2022-06-01 19:42:40 +0200

    Fixed a bug that caused a loop when trying to compile files. Now you can import AquacropModel directly.

--- aquacrop/__init__.py
+++ aquacrop/__init__.py
@@ -1,9 +1,11 @@
-
-from .entities.soil import Soil
-from .entities.crop import  Crop
-from .entities.inititalWaterContent import InitialWaterContent
-from .entities.irrigationManagement import IrrigationManagement
-from .entities.fieldManagement import FieldMngt
-from .entities.groundWater import  GroundWater
-from .utils.prepare_weather import prepare_weather
-from .utils.data import get_filepath
+import sys
+if not '-m' in sys.argv:
+    from .core import AquaCropModel
+    from .entities.soil import Soil
+    from .entities.crop import  Crop
+    from .entities.inititalWaterContent import InitialWaterContent
+    from .entities.irrigationManagement import IrrigationManagement
+    from .entities.fieldManagement import FieldMngt
+    from .entities.groundWater import  GroundWater
+    from .utils.prepare_weather import prepare_weather
+    from .utils.data import get_filepath
