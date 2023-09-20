ab5019bdf1c6d257c792482c9495e9add90e0d29
Chris <chris.bowden@live.co.uk>
2023-09-20 12:17:33 +0100

    Update testing plots, revert model changes
    Got AttributeError: AquaCropModel object has no attribute '_outputs'...reverting last change to see if it fixes.

--- tests/test_groundwater.py
+++ tests/test_groundwater.py
@@ -25,21 +25,24 @@ irr_mngt = IrrigationManagement(irrigation_method=1)
 
 y_axis = 'Infl' # canopy_cover
 
-model1 = AquaCropModel(sim_start_time=f'{1979}/10/01',
-                      sim_end_time=f'{1985}/05/30',
-                      weather_df=weather_data,
-                      soil=sandy_loam,
-                      crop=wheat,
-                      irrigation_management=irr_mngt,
-                      initial_water_content=InitWC)
+# SINGLE MODEL TESTING
+# model1 = AquaCropModel(sim_start_time=f'{1979}/10/01',
+#                       sim_end_time=f'{1985}/05/30',
+#                       weather_df=weather_data,
+#                       soil=sandy_loam,
+#                       crop=wheat,
+#                       irrigation_management=irr_mngt,
+#                       initial_water_content=InitWC)
 
-# run model till termination
-model1.run_model(till_termination=True)
+# # run model till termination
+# model1.run_model(till_termination=True)
 
-print(model1._outputs.water_storage)
+# print(model1._outputs.water_storage)
 
-sns.boxplot(data=pd.DataFrame(model1._outputs.water_flux),x='time_step_counter',y='Infl')
+# SINGLE MODEL SINGLE PLOT
+# sns.boxplot(data=pd.DataFrame(model1._outputs.water_flux),x='time_step_counter',y='Infl')
 
+# SINGLE MODEL MULTI-PLOT
 # fig,ax=plt.subplots(2,2,figsize=(12,14))
 
 # sns.boxplot(data=pd.DataFrame(model1._outputs.water_storage),x='time_step_counter',y='th1',ax=ax[0,0])
@@ -47,6 +50,7 @@ sns.boxplot(data=pd.DataFrame(model1._outputs.water_flux),x='time_step_counter',
 # sns.boxplot(data=pd.DataFrame(model1._outputs.water_storage),x='time_step_counter',y='th3',ax=ax[1,0])
 # sns.boxplot(data=pd.DataFrame(model1._outputs.water_storage),x='time_step_counter',y='th4',ax=ax[1,1])
 
+# MULTIMODEL TESTING
 model1 = AquaCropModel(sim_start_time=f'{1979}/10/01',
                       sim_end_time=f'{1985}/05/30',
                       weather_df=weather_data,
@@ -54,4 +58,20 @@ model1 = AquaCropModel(sim_start_time=f'{1979}/10/01',
                       crop=wheat,
                       irrigation_management=irr_mngt,
                       initial_water_content=InitWC,
-                      groundwater=GroundWater(method='Variable'))
+                      off_season=False)
+                    #   groundwater=GroundWater(water_table="N", method="Constant", dates=[], values=[]))
+
+model2 = AquaCropModel(sim_start_time=f'{1979}/10/01',
+                      sim_end_time=f'{1985}/05/30',
+                      weather_df=weather_data,
+                      soil=sandy_loam,
+                      crop=wheat,
+                      irrigation_management=irr_mngt,
+                      initial_water_content=InitWC,
+                      groundwater=GroundWater(water_table="N", method="Variable", dates=[], values=[]))
+
+# MULTIMODEL MULTI PLOT
+# fig,ax=plt.subplots(2,1,figsize=(12,14))
+print(model1._outputs)
+sns.boxplot(data=pd.DataFrame(model1._outputs.water_flux),x='time_step_counter',y='z_gw') # ,ax=ax[0]
+# sns.boxplot(data=pd.DataFrame(model2._outputs.water_flux),x='time_step_counter',y='z_gw',ax=ax[1])
