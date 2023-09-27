25b70ad4eb0ba828cd17dd4e12a36d2794ebd4a7
Chris <chris.bowden@live.co.uk>
2023-09-27 15:09:11 +0100

    Added ref to fallow field mngt in main text

--- tests/test_off_season.py
+++ tests/test_off_season.py
@@ -8,7 +8,7 @@ import seaborn as sns
 from aquacrop import AquaCropModel, Soil, Crop, InitialWaterContent, IrrigationManagement, FieldMngt
 from aquacrop.utils import prepare_weather, get_filepath
 
-filepath=get_filepath('champion_climate.txt')
+filepath=get_filepath('tunis_climate.txt')
 
 weather_data = prepare_weather(filepath)
 
@@ -16,26 +16,26 @@ weather_data = prepare_weather(filepath)
 sandy_loam = Soil(soil_type='SandyLoam')
 
 # crops
-wheat = Crop('Maize', planting_date='10/01')
+wheat = Crop('Wheat', planting_date='10/01')
 
 # IWC
 InitWC = InitialWaterContent(value=['FC'])
 
 # irr management
-irr_mngt = IrrigationManagement(irrigation_method=0)
+irr_mngt = IrrigationManagement(irrigation_method=1)
 
 # fallow field management
 fal_fld = FieldMngt(mulches=True,
-                    mulch_pct=100,
+                    mulch_pct=20,
                     bunds=True,
-                    bund_water=10,
+                    bund_water=30,
                     z_bund=1)
 
 y_axis = 'Wr' # canopy_cover, surface_storage, Runoff, Es, Infl
 
 # combine into aquacrop model and specify start and end simulation date
 model1 = AquaCropModel(sim_start_time=f'{1982}/10/01',
-                      sim_end_time=f'{1985}/05/30',
+                      sim_end_time=f'{1986}/05/30',
                       weather_df=weather_data,
                       soil=sandy_loam,
                       crop=wheat,
@@ -49,7 +49,7 @@ model1.run_model(till_termination=True)
 # print(model1._outputs.crop_growth)
 
 model2 = AquaCropModel(sim_start_time=f'{1982}/10/01',
-                      sim_end_time=f'{1985}/05/30',
+                      sim_end_time=f'{1986}/05/30',
                       weather_df=weather_data,
                       soil=sandy_loam,
                       crop=wheat,
@@ -60,14 +60,16 @@ model2 = AquaCropModel(sim_start_time=f'{1982}/10/01',
 
 model2.run_model(till_termination=True)
 
+# For interactive running plots
 # fig,ax=plt.subplots(2,1,figsize=(12,14))
 
 # sns.scatterplot(data=pd.DataFrame(model1._outputs.water_flux),x='time_step_counter',y=y_axis, ax=ax[0])
 # sns.scatterplot(data=pd.DataFrame(model2._outputs.water_flux),x='time_step_counter',y=y_axis, ax=ax[1])
-model_out=pd.DataFrame(model2._outputs.water_flux)
 
+model1_water=pd.DataFrame(model1._outputs.water_flux)
+model2_water=pd.DataFrame(model2._outputs.water_flux)
 # Test all outputs
-all_water_flux = [
+water_flux = [
                 "Wr",
                 "z_gw",
                 "surface_storage",
@@ -83,10 +85,52 @@ all_water_flux = [
                 "TrPot",
             ]
 
-for y_var in all_water_flux:
+for y_var in water_flux:
     plt.figure()
-    plot=sns.scatterplot(data=model_out,
-                        x='time_step_counter',
-                        y=y_var)
-    fig=plot.get_figure()
-    fig.savefig(f'../plots/off_season/{y_var}_2.png')
+    fig,ax=plt.subplots(2,1,figsize=(12,14))
+    sns.scatterplot(data=model1_water,
+                    x='time_step_counter',
+                    y=y_var,
+                    ax=ax[0])
+    sns.scatterplot(data=model2_water,
+                    x='time_step_counter',
+                    y=y_var,
+                    ax=ax[1])
+    
+    fig.savefig(f'../plots/off_season/water_flux_fallow/{y_var}.png')
+    fig.clf()
+    plt.cla()
+    plt.close(fig)
+
+model1_crop=pd.DataFrame(model1._outputs.crop_growth)
+model2_crop=pd.DataFrame(model2._outputs.crop_growth)
+
+crop_growth=[
+                "gdd",
+                "gdd_cum",
+                "z_root",
+                "canopy_cover",
+                "canopy_cover_ns",
+                "biomass",
+                "biomass_ns",
+                "harvest_index",
+                "harvest_index_adj",
+                "yield_",
+            ]
+
+for y_var in crop_growth:
+    plt.figure()
+    fig,ax=plt.subplots(2,1,figsize=(12,14))
+    sns.scatterplot(data=model1_crop,
+                    x='time_step_counter',
+                    y=y_var,
+                    ax=ax[0])
+    sns.scatterplot(data=model2_crop,
+                    x='time_step_counter',
+                    y=y_var,
+                    ax=ax[1])
+    
+    fig.savefig(f'../plots/off_season/crop_growth_fallow/{y_var}.png')
+    fig.clf()
+    plt.cla()
+    plt.close(fig)
