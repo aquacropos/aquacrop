2c59072dda5db8ec5bc2cbf5abaa1b3e1a74aed6
Chris <chris.bowden@live.co.uk>
2023-12-01 09:53:51 +0000

    Removed lines 83-86 of check_gw_table

--- aquacrop/solution/check_groundwater_table.py
+++ aquacrop/solution/check_groundwater_table.py
@@ -80,10 +80,10 @@ def check_groundwater_table(
                 NewCond_WTinSoil = True
 
         # If water table is in soil profile, adjust water contents
-        if NewCond_WTinSoil == True:
-            idx = np.argwhere(zMid >= NewCond_zGW).flatten()[0]
-            for ii in range(idx, len(prof.Comp)):
-                NewCond_th[ii] = prof.th_s[ii]
+        # if NewCond_WTinSoil == True:
+        #     idx = np.argwhere(zMid >= NewCond_zGW).flatten()[0]
+        #     for ii in range(idx, len(prof.Comp)):
+        #         NewCond_th[ii] = prof.th_s[ii]
 
         # Adjust compartment field capacity
         compi = len(prof.Comp) - 1
